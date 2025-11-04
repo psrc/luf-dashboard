ee6957d60a59b290d5015cf8deb0294739dab0c3
Hana Sevcikova <hanas@uw.edu>
2025-11-04 17:53:58 +0100

    fixed order of RGs

--- modules/top-sheet-rgs.R
+++ modules/top-sheet-rgs.R
@@ -46,7 +46,7 @@ dt_rgs_server <- function(id, paths, runs, tsyear, baseyear) {
           cols <- setdiff(colnames(datatable), "city_id")
           datatable[city.lookup, `:=`(rgs_id = i.rgs_id, county_id = i.county_id, county = i.county), on = "city_id"]
           
-          datatable.rgs <- datatable[, lapply(.SD, sum), by = "rgs_id", .SDcols = cols][order(rgs_id)]
+          datatable.rgs <- datatable[, lapply(.SD, sum), by = "rgs_id", .SDcols = cols]
           colnames(datatable.rgs)[2:ncol(datatable.rgs)] <- str_replace(colnames(datatable.rgs)[2: ncol(datatable.rgs)], '\\w+_', 'yr') # rename columns
           colnames(datatable.rgs)[1] <- str_replace(colnames(datatable.rgs)[1], '\\w+_', 'name_')
           datatable.rgs$indicator <- switch(attribute[i],
@@ -89,7 +89,7 @@ dt_rgs_server <- function(id, paths, runs, tsyear, baseyear) {
           cols <- setdiff(colnames(datatable), "city_id")
           datatable[city.lookup, `:=`(rgs_id = i.rgs_id, county_id = i.county_id, county = i.county), on = "city_id"]
           
-          datatable.cnty.rgs <- datatable[, lapply(.SD, sum), by = c("county", "rgs_id"), .SDcols = cols][order(county, rgs_id)]
+          datatable.cnty.rgs <- datatable[, lapply(.SD, sum), by = c("county", "rgs_id"), .SDcols = cols]
           colnames(datatable.cnty.rgs)[3:ncol(datatable.cnty.rgs)] <- str_replace(colnames(datatable.cnty.rgs)[3: ncol(datatable.cnty.rgs)], '\\w+_', 'yr') # rename columns
           colnames(datatable.cnty.rgs)[2] <- str_replace(colnames(datatable.cnty.rgs)[2], '\\w+_', 'name_')
           datatable.cnty.rgs$indicator <- switch(attribute[i],
@@ -125,9 +125,10 @@ dt_rgs_server <- function(id, paths, runs, tsyear, baseyear) {
                      ][indicator == 'Total Population', indicator := 'Population']
       
       tm <- melt.data.table(t,
-                            id.vars = c("name", "indicator", "run"),
+                            id.vars = c("name_id", "name", "indicator", "run"),
                             measure.vars = str_subset(colnames(t), "^yr.*"),
                             variable.name = "year")
+
       tm[, year := str_extract(year, "\\d+")]
       t1 <- tm[((run %in% runs[1]) & (year %in% c(b1, tsyear))) | ((run %in% runs[2]) & (year %in% c(b2, tsyear)))]
     })
@@ -146,9 +147,9 @@ dt_rgs_server <- function(id, paths, runs, tsyear, baseyear) {
       t <- rgsdt[(indicator == 'Total Population' | indicator == 'Employment')
       ][indicator == 'Total Population', indicator := 'Population']
       
-      t[, name := paste(county, name, sep = ": ")][, county := NULL]
+      #t[, name := paste(county, name, sep = ": ")][, county := NULL]
       tm <- melt.data.table(t,
-                            id.vars = c("name", "indicator", "run"),
+                            id.vars = c("name_id", "county", "name", "indicator", "run"),
                             measure.vars = str_subset(colnames(t), "^yr.*"),
                             variable.name = "year")
       tm[, year := str_extract(year, "\\d+")]
@@ -168,7 +169,7 @@ dt_rgs_server <- function(id, paths, runs, tsyear, baseyear) {
       b2 <- baseyear[run == runs[2], .(baseyear)][[1]] |> str_extract("\\d+")
       cols <- paste0("yr", c(b1, b2, tsyear))
       
-      t <- dcast.data.table(tsRGs, name ~ year + indicator + run, value.var = "value")
+      t <- dcast.data.table(tsRGs, name_id + name ~ year + indicator + run, value.var = "value")[, name_id := NULL]
       setnames(t, "name", "Name")
       t1 <- create.exp.tsTable(t, runs, tsyear, baseyear)
       sketch <- sketch.expanded(colnames(t1)[1], b1, b2, tsyear, runnames[1], runnames[2])
@@ -188,8 +189,8 @@ dt_rgs_server <- function(id, paths, runs, tsyear, baseyear) {
       b2 <- baseyear[run == runs[2], .(baseyear)][[1]] |> str_extract("\\d+")
       cols <- paste0("yr", c(b1, b2, tsyear))
       
-      t <- dcast.data.table(tsRGs, name ~ year + indicator + run, value.var = "value")
-      setnames(t, "name", "Name")
+      t <- dcast.data.table(tsRGs, county + name_id + name ~ year + indicator + run, value.var = "value")
+      t[, Name := paste(county, name, sep = ": ")][, `:=`(county = NULL, name_id = NULL, name = NULL)]
       t1 <- create.exp.tsTable(t, runs, tsyear, baseyear)
       sketch <- sketch.expanded(colnames(t1)[1], b1, b2, tsyear, runnames[1], runnames[2])
       create.DT.expanded(t1, sketch)
